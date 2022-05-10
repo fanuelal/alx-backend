5244aa44a254bf69d3381983410474ed73b33e1c
fanuelal <fanuelalmawstop@gmail.com>
2022-05-10 05:32:28 -0700

    pycodestyle fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,12 +6,13 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-@app.route('/')
 
+
+@app.route('/')
 def basicsApp():
     """Basic flask app function"""
-
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
