2da989ab868375b8f5bfe445e442212e930a22c8
fanuelal <fanuelalmawstop@gmail.com>
2022-05-10 08:12:42 -0700

    locale with url fi

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pthon3
+"""Module Force locale"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ App config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    ''' return best languages '''
+    lang = request.args.get('locale')
+    if lang:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def rootApp():
+    """ return 2-index"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
