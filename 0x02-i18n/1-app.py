62c7559675686f0a3cfcc79c8425e7abd71fd2b8
fanuelal <fanuelalmawstop@gmail.com>
2022-05-10 05:57:38 -0700

    bable

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module  Basic Babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+bable = Babel(app)
+
+
+class Config:
+    """configure available"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "ën"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def RootApp():
+    """returns 1-index html file"""
+    return render_template('1_index.html')
+
+
+if __name__ == "__main__":
+    app.run()
