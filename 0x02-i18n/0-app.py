07431147016ed45f7948197e9ab27b89fbf25ab1
fanuelal <fanuelalmawstop@gmail.com>
2022-05-10 05:14:29 -0700

    basic app naming error

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Module Basic Flask app"""
+
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 @app.route('/')
 
-def rootApp():
+def basicsApp():
     """Basic flask app function"""
 
     return render_template('0-index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
