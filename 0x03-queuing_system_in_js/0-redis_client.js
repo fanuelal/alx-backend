3e3ff3f2d2882ed6c87678a5881c826f831e90b5
fanuelal <fanuelalmawstop@gmail.com>
2022-06-07 09:42:16 +0000

    redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+    if (err) console.log(`Redis client not connected to the server: ${err}`);
+	 }).on('ready', () => {
+	     console.log('Redis client connected to the server');
+});
