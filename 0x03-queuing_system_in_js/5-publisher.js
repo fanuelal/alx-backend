6af8add33b94eabf84196257e8345bb30c8abf56
fanuelal <fanuelalmawstop@gmail.com>
2022-06-07 11:10:31 +0000

    publisher

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+    if (err) console.log(`Redis client not connected to the server: ${err}`);
+         }).on('ready', () => {
+             console.log('Redis client connected to the server');
+	 });
+
+function publishMessage(message, time){
+    setTimeout(()=> {
+	console.log(`About to send MESSAGE ${message}`);
+	client.publish('holberton school channel', message)
+    }, time);
+}
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
